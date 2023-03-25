3629a20db796100a56242a2d0938b3a860c5e350
Rubrik25 <75697272+Rubrik25@users.noreply.github.com>
2023-03-25 11:55:20 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,33 @@
-$(document).ready(function() {
-        
-        // При нажатии на кнопку
-        $("#toggle-button").click(function() {
-          var parsed = parser($("#input-field").val());
-          $(".lib").html(parsed);
-          // Переключение элементов
-          $(".editor, .lib").toggleClass("hidden");          
-        });
-        
-      });
-function parser(inputed) {
-  inputed = inputed.split("\n");
-  inputed = inputed.map(i => {
+// Готовность документа
+$(document).ready(() => {
+  // Сохраняем ссылки на элементы jQuery в переменные
+  const $toggleButton = $("#toggle-button");
+  const $inputField = $("#input-field");
+  const $lib = $(".lib");
+  const $editorLibToggle = $(".editor,.lib");
+  
+  // При нажатии на кнопку
+  $toggleButton.click(() => {
+    // Парсим введенный текст
+    const parsed = parser($inputField.val());
+    // Записываем результат в элемент с классом "lib"
+    $lib.html(parsed);
+    // Переключение элементов
+    $editorLibToggle.toggleClass("hidden");
+  });
+});
+
+// Функция парсинга текста
+const parser = inputed => {
+  let parsed = inputed.split("\n");
+  parsed = parsed.map(i => {
     if (i.trim() !== "") {
       return `<p>${i}</p>`;
     } else {
       return "&nbsp;";
     }
   });
-  inputed = inputed.map(i => i.replace("[[checkbox]]", '<input type="checkbox">'));
-  inputed = inputed.join("");
-  return inputed;
-}
-
+  parsed = parsed.map(i => i.replace("[[checkbox]]", '<input type="checkbox">'));
+  parsed = parsed.join("");
+  return parsed;
+};
