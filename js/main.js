2d2f50d56b4504e27c7cb8d18fee77b9e48299b6
Rubrik25 <75697272+Rubrik25@users.noreply.github.com>
2023-04-05 14:22:37 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ $(document).ready(() => {
   const $paragraphSpacing = $("#paragraph-spacing");
   
 // Нажатие на кнопку "Назад"
-  $backButton.onclick(function() {
+  $backButton.click(() => {
     $settings.addClass("hidden");
     $editor.addClass("hidden");
     $lib.removeClass("hidden");
@@ -29,14 +29,14 @@ $(document).ready(() => {
   });
 
 // Нажатие на кнопку "Редактор"
-  $editorButton.onclick(function() {
+  $editorButton.click(() => {
     $settings.addClass("hidden");
     $lib.addClass("hidden");
     $editor.removeClass("hidden");
   });
 
 // Нажатие на кнопку "Настройки"
-  $settingsButton.onclick(function() {
+  $settingsButton.click(() => {
     $editor.addClass("hidden");
     $lib.addClass("hidden");
     $settings.removeClass("hidden");
